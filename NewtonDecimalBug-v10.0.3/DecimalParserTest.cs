ce021e80460392ff6eff7311c5860567a15109b9
rodrigobrito <rodrigo@britoebrito.com.br>
2019-01-10 16:29:15 -0200

    Newtonsoft inconsistency betwen versions.

---
+++ NewtonDecimalBug-v10.0.3/DecimalParserTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NewtonDecimalBug_v10._0._3
+{
+    extern alias Newtonsoft10x;
+    extern alias Newtonsoft11x;
+
+    [TestClass]
+    public class DecimalParserTest
+    {
+        [TestMethod]
+        public void TestNewtonsoftParser()
+        {
+            var v = 15.1200m;
+
+            var v10Serialized = Newtonsoft10x::Newtonsoft.Json.JsonConvert.SerializeObject(v);
+            var v10Deserialized = Newtonsoft10x::Newtonsoft.Json.JsonConvert.DeserializeObject<decimal>(v10Serialized);
+
+            var v11Serialized = Newtonsoft11x::Newtonsoft.Json.JsonConvert.SerializeObject(v);
+            var v11Deserialized = Newtonsoft11x::Newtonsoft.Json.JsonConvert.DeserializeObject<decimal>(v11Serialized);
+
+            Assert.AreEqual(v10Serialized, "15.1200");
+            Assert.AreEqual(v10Deserialized.ToString(CultureInfo.InvariantCulture), "15.12"); // Inconsistency between versions
+
+            Assert.AreEqual(v11Serialized, "15.1200");
+            Assert.AreEqual(v11Deserialized.ToString(CultureInfo.InvariantCulture), "15.1200"); //Inconsistency between versions
+        }
+    }
+}
---
+++ NewtonDecimalBug-v10.0.3/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NewtonDecimalBug-v10.0.3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NewtonDecimalBug-v10.0.3")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a002fee2-f4d7-4b3e-a566-3d7c8ee128d4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
